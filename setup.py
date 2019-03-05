38d6d56730dab00f45b0e485f0d2ad1a7556fbe9
Misa <arensiatik@gmail.com>
2019-03-04 20:55:53 -0600

    Change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pushcrew',
-    version='0.0.1dev0',
+    version='0.0.2dev0',
     description='Python wrapper around the Pushcrew API',
     author='Misa G.',
     author_email='hi@misalabs.com',
